66291d63710867519dd0ef5cf07cc4f0d85c0ff5
madmdx07 <mahessdhant@gmail.com>
2023-12-29 21:32:58 +0545

    Add project files.

---
+++ wwwroot/js/app-component.js
@@ -0,0 +1,24 @@
+class pageModel {
+    constructor() {
+        this.currentComponent = 'home-component';
+    }
+}
+
+Vue.component('home-component',
+    function (resolve, reject) {
+        $.get('./html/index.html').then(function (res) {
+            resolve({
+                template: res,
+                data: function () {
+                    return {
+
+                    };
+                },
+                methods: {
+                },
+                mounted: function () {
+
+                },
+            })
+        })
+    })

1368d44a94192dcaade4e409ba06d6d355fd9f9b
madmdx07 <mahessdhant@gmail.com>
2023-12-31 23:46:27 +0545

    urls, db connection

---
+++ wwwroot/js/app-url.js
@@ -0,0 +1,6 @@
+var apiBaseUrl = "/api/";
+var apiControlAdminBaseUrl = apiBaseUrl + "control/admin/"
+var apiControlAdminUrl = {
+    imageList: apiControlAdminBaseUrl + "image-list",
+    addImage: apiControlAdminBaseUrl + "add-image"
+}

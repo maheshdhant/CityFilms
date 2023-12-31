938ea9b116cea2ba9b312950ce3d95115553ae7a
madmdx07 <mahessdhant@gmail.com>
2023-12-31 23:45:35 +0545

    admin api

---
+++ Controllers/Api/Control/ApiAdminController.cs
@@ -0,0 +1,26 @@
+﻿using CityFlims.Entity;
+using CityFlims.Models;
+using CityFlims.Services.Control.AdminServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityFlims.Controllers.Api.Control
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApiAdminController : ControllerBase
+    {
+        private IAdminServices _actionServices;
+        public ApiAdminController(IAdminServices actionServices)
+        {
+            _actionServices = actionServices;
+        }
+
+        [HttpGet("image-list")]
+        public async Task<IEnumerable<ImageModel>> GetImages()
+        {
+            var serviceResponse = await _actionServices.GetImages();
+            return serviceResponse;
+        }
+    }
+}

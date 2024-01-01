9cac26ae757c907feea06ec59dcec799b69fca7b
madmdx07 <mahessdhant@gmail.com>
2024-01-01 23:59:43 +0545

    admin dashboard component and urls

--- wwwroot/js/app-component.js
+++ wwwroot/js/app-component.js
@@ -4,6 +4,19 @@ class pageModel {
     }
 }
 
+class adminDashboardModel {
+    constructor() {
+        this.imageList = [new imageModel()];
+    }
+}
+
+class imageModel {
+    constructor() {
+        this.imageName = '';
+        this.imageLocation = '';
+    }
+}
+
 Vue.component('home-component',
     function (resolve, reject) {
         $.get('./html/index.html').then(function (res) {
@@ -15,10 +28,40 @@ Vue.component('home-component',
                     };
                 },
                 methods: {
-
+                    goToAdmin: function () {
+                        window.location = "/#admin/dashboard";
+                    }
                 },
                 mounted: function () {
                     getYear()
+                },
+            })
+        })
+    })
+
+Vue.component('admin-dashboard-component',
+    function (resolve, reject) {
+        $.get(htmlControlAdminUrl.adminDashboard).then(function (res) {
+            resolve({
+                template: res,
+                data: function () {
+                    return {
+                        page: new adminDashboardModel(),
+                    };
+                },
+                methods: {
+                    getImageList: function () {
+                        let currentObj = this;
+                        var response = getRequest(apiControlAdminUrl.imageList);
+
+                        response.done(function (response) {
+                            currentObj.page.imageList = response
+                        })
+
+                    }
+                },
+                mounted: function () {
+                    
                 },
             })
         })
--- wwwroot/js/app-url.js
+++ wwwroot/js/app-url.js
@@ -1,6 +1,18 @@
 var apiBaseUrl = "/api/";
+var htmlBaseUrl = "/html/";
+var htmlControlAdminBaseUrl = htmlBaseUrl + "control/admin/";
+var htmlControlAdminUrl = {
+    adminDashboard: htmlControlAdminBaseUrl + "admindashboard.html"
+}
 var apiControlAdminBaseUrl = apiBaseUrl + "control/admin/"
 var apiControlAdminUrl = {
-    imageList: apiControlAdminBaseUrl + "image-list",
+    imageList: apiControlAdminBaseUrl + "images-list",
     addImage: apiControlAdminBaseUrl + "add-image"
+}
+
+
+// hashValue
+var hashValue = {
+    home: "#index",
+    adminDashboard: "#admin/dashboard",
 }

13dcd9aaed7276ae845cb29ad747f5eee17ac543
madmdx07 <mahessdhant@gmail.com>
2024-01-13 23:37:53 +0545

    home api

---
+++ Controllers/Api/Control/ApiHomeController.cs
@@ -0,0 +1,26 @@
+﻿using CityFilms.Entity;
+using CityFilms.Models;
+using CityFilms.Services.Control.HomeServices;
+using Microsoft.AspNetCore.Mvc;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace CityFilms.Controllers.Api.Control
+{
+    [Route("api/control/home")]
+    [ApiController]
+    public class ApiHomeController : ControllerBase
+    {
+        private IHomeServices _actionServices;
+        public ApiHomeController(IHomeServices actionServices)
+        {
+            _actionServices = actionServices;
+        }
+        
+            [HttpGet("current-bg")]
+        public async Task<IActionResult> GetCurrentBackground()
+        {
+            var serviceResponse = await _actionServices.GetCurrentBackground();
+            return Ok(serviceResponse);
+        }
+    }
+}  

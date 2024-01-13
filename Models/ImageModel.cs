0797f49dd725e88b251b697170423455303d743b
madmdx07 <mahessdhant@gmail.com>
2024-01-13 23:41:09 +0545

    link to background image

--- Models/ImageModel.cs
+++ Models/ImageModel.cs
@@ -4,9 +4,11 @@ namespace CityFilms.Models
 {
     public class ImageModel
     {
+        public int ImageId { get; set; }
         public string? ImageName { get; set; }
         public string? ImageLocation { get; set; }
         public IFormFile? ImageFile { get; set; }
         public int? ImageTypeId { get; set; }
+        public bool? IsSelected { get; set; }
     }
 }

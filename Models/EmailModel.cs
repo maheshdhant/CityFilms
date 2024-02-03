1770bcdb4919c176be763d801a0e21fb7c7b3a7b
madmdx07 <mahessdhant@gmail.com>
2024-02-04 00:00:01 +0545

    email

---
+++ Models/EmailModel.cs
@@ -0,0 +1,23 @@
+﻿namespace CityFilms.Models
+{
+    public class EmailModel
+    {
+        public string To { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public IFormFile Attachment { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public string Sender { get; set; }
+    }
+
+    public class EmailConfig
+    {
+        public string Email { get; set; }
+        public string User { get; set; }
+        public string Password { get; set; }
+        public string Host { get; set; }
+        public int Port { get; set; }
+    }
+}
--- Services/Control/HomeServices/HomeServices.cs
+++ Services/Control/HomeServices/HomeServices.cs
@@ -2,15 +2,20 @@
 using CityFilms.Models;
 using CityFilms.Models.Response;
 using Microsoft.EntityFrameworkCore;
+using System.Net.Mail;
+using System.Net;
+using Microsoft.Extensions.Options;
 
 namespace CityFilms.Services.Control.HomeServices
 {
     public class HomeServices: IHomeServices
     {
         private readonly CityfilmsDataContext _context;
-        public HomeServices(CityfilmsDataContext context)
+        private readonly EmailConfig _mailSettings;
+        public HomeServices(CityfilmsDataContext context, IOptions<EmailConfig> mailSettings)
         {
             _context = context;
+            _mailSettings = mailSettings.Value;
         }
         public async Task<ServiceResponse<dynamic>> GetCompanyLogo()
         {
@@ -47,6 +52,34 @@ namespace CityFilms.Services.Control.HomeServices
             {
                 Data = model,
             };
+        }
+        public async Task<bool> EmailSender(EmailModel model)
+        {
+            model.Sender = _mailSettings.Email;
+            model.Password = _mailSettings.Password;
+            var a = true;
+            using (MailMessage mm = new MailMessage(model.Sender, model.To))
+            {
+                mm.Subject = model.Subject;
+                mm.Body = model.Body;
+                if (model.Attachment != null && model.Attachment.Length > 0)
+                {
+                    string fileName = Path.GetFileName(model.Attachment.FileName);
+                    mm.Attachments.Add(new Attachment(model.Attachment.OpenReadStream(), fileName));
+                }
+                mm.IsBodyHtml = false;
+                using (SmtpClient smtp = new SmtpClient())
+                {
+                    smtp.Host = _mailSettings.Host;
+                    smtp.EnableSsl = true;
+                    NetworkCredential NetworkCred = new NetworkCredential(model.Sender, model.Password);
+                    smtp.UseDefaultCredentials = false;
+                    smtp.Credentials = NetworkCred;
+                    smtp.Port = _mailSettings.Port;
+                    smtp.Send(mm);
+                }
+            }
+            return true;
         }
     }
 }

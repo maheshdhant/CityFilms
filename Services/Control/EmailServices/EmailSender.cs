7124cec202751309db89986fef747cdb21394c0d
madmdx07 <mahessdhant@gmail.com>
2024-02-05 23:27:33 +0545

    email services

---
+++ Services/Control/EmailServices/EmailSender.cs
@@ -0,0 +1,27 @@
+﻿using CityFilms.Models;
+using CityFilms.Models.Response;
+using System.Net;
+using System.Net.Mail;
+
+namespace CityFilms.Services.Control.EmailServices
+{
+    public class EmailSender : IEmailSender
+    {
+        public async Task<ServiceResponse<dynamic>> SendEmail(EmailModel model)
+        {
+            var mail = "mahessdhant@gmail.com";
+            var pw = "tsbr ksoe qzno ugol";
+            var client = new SmtpClient("smtp.gmail.com")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential(mail, pw),
+                EnableSsl = true,
+            };
+            client.Send(mail, model.Email, model.Subject, model.Body);
+            return new ServiceResponse<dynamic>()
+            {
+                Data = "Booking Success!!"
+            };
+        }
+    }
+}
---
+++ Services/Control/EmailServices/IEmailSender.cs
@@ -0,0 +1,10 @@
+﻿using CityFilms.Models;
+using CityFilms.Models.Response;
+
+namespace CityFilms.Services.Control.EmailServices
+{
+    public interface IEmailSender
+    {
+        Task<ServiceResponse<dynamic>> SendEmail(EmailModel model);
+    }
+}

a355736797837f71f3b1173d294c14710fd779a2
madmdx07 <mahessdhant@gmail.com>
2024-02-07 22:37:58 +0545

    contactLog and mailLog model

---
+++ Models/ContactLogModel.cs
@@ -0,0 +1,13 @@
+﻿namespace CityFilms.Models
+{
+    public class ContactLogModel
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Phone { get; set; }
+        public string? Email { get; set; }
+        public string? Subject { get; set; } = "Booking";
+        public string? Message { get; set; }
+        public DateTime? CreatedDate { get; set; }
+    }
+}
---
+++ Models/MailLogModel.cs
@@ -0,0 +1,13 @@
+﻿namespace CityFilms.Models
+{
+    public class MailLogModel
+    {
+        public string? Subject { get; set; }
+
+        public string? SentBy { get; set; }
+
+        public string? SentTo { get; set; }
+
+        public DateTime? CreatedDate { get; set; }
+    }
+}

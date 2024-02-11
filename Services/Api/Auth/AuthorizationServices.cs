9af19c7c7a07fa849f0f0b8fc0ac39a1afb7b20f
madmdx07 <mahessdhant@gmail.com>
2024-02-11 15:41:14 +0545

    user register services

---
+++ Services/Api/Auth/AuthorizationServices.cs
@@ -0,0 +1,82 @@
+﻿using CityFilms.Entity;
+using CityFilms.Models;
+using CityFilms.Models.Response;
+
+namespace CityFilms.Services.Api.Auth
+{
+    public class AuthorizationServices : IAuthorizationServices
+    {
+
+        private readonly string _secret;
+        private readonly string _expDate;
+        private IConfiguration _config;
+        public AuthorizationServices(IConfiguration config) 
+        {
+            _config = config;
+            _secret = config.GetSection("JwtConfig").GetSection("Secret").Value;
+            _expDate = config.GetSection("JwtConfig").GetSection("ExpirationInMinutes").Value;
+        }
+
+        public async Task<ServiceResponse<string>> RegisterUser(NewUserRegisterModel model)
+        {
+            Random rnd = new Random();
+            var userName = model.UserName;
+            var email = model.Email;
+            var password = model.Password;
+            if (userName.Length < 4)
+            {
+                return new ServiceResponse<string>() { Data = "Username must be longer than 4 characters!" };
+            }
+
+            // check if email exists
+            using var ent = new CityfilmsDataContext();
+            var emailcheck = await ent.UserProfiles.AnyAsync(x => x.Email == model.Email);
+            if (emailcheck == true)
+            {
+                return new ServiceResponse<string>() { Data = "Email already registered!" };
+            }
+
+            // check if username exists
+            var checkUser = await ent.UserProfiles.FirstOrDefaultAsync(x => x.UserName.ToLower() == userName.ToLower());
+            if (checkUser != null)
+            {
+                return new ServiceResponse<string>() { Data = "Username not available!" };
+            }
+
+            var pass = new UserPassword();
+            var salt = pass.GenerateSalt(userName);
+            var hashPass = pass.GetHmac(password, salt);
+            var userId = Guid.NewGuid();
+
+            var user = new User()
+            {
+                Password = hashPass,
+                PasswordSalt = salt,
+                IsActive = false,
+                IsLockedOut = false,
+                CreateDate = DateTime.UtcNow,
+                FailedPasswordAttemptCount = 0,
+                UserId = userId,
+                UserTypeId = null,
+            };
+            ent.Users.Add(user);
+
+            var profile = new UserProfile()
+            {
+                UserId = userId,
+                Email = email,
+                UserName = userName,
+                UserProfileId = Guid.NewGuid(),
+            };
+            ent.UserProfiles.Add(profile);
+
+            await ent.SaveChangesAsync();
+
+
+            return new ServiceResponse<string>()
+            {
+                Data = "Registration completed!",
+            };
+        }
+    }
+}
---
+++ Services/Api/Auth/IAuthorizationServices.cs
@@ -0,0 +1,10 @@
+﻿using CityFilms.Models.Response;
+using CityFilms.Models;
+
+namespace CityFilms.Services.Api.Auth
+{
+    public interface IAuthorizationServices
+    {
+        Task<ServiceResponse<string>> RegisterUser(NewUserRegisterModel model);
+    }
+}
